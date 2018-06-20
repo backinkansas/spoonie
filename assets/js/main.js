b7d1739ab38c06d01b43012827e157ab6a0642db
backinkansas <milena.martinsc@gmail.com>
2018-06-20 16:06:42 -0300

    Interaction message completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,14 @@
 const badges = document.querySelectorAll('.badge')
 let points = document.getElementById('score_p')
-let modal = document.getElementById('myModal')
-let buttonOut = document.querySelector('#button_out')
-let buttonEnd = document.querySelector('#button_end')
+let modal = document.getElementById('my_modal')
+let modalAlert = document.getElementById('modal_alert')
+const buttonOut = document.querySelector('#button_out')
+const buttonEnd = document.querySelector('#button_end')
+let interactionText = document.querySelector('#interactionText')
+let selected = []
+const tryWin = document.getElementById('try_win')
+const buttonGiveUp = document.getElementById('button_giveup')
+const buttonContinue = document.getElementById('button_continue')
 
 function removeSpoon() {
     let spoonContainer = document.querySelector('#all_spoons')
@@ -24,6 +30,7 @@ function addPointChangeBadge(img) {
     img.setAttribute('src', './assets/img/medal.svg')
     points.textContent = parseInt(points.textContent) + 1
     removeSpoon()
+    selected.push(img.getAttribute('value'))
 }
 
 function removePointChangeBadge(img) {
@@ -35,10 +42,19 @@ function alertMissingCurrency() {
     modal.style.display = "block"
 }
 
+function verify(action) {
+    return selected.includes(action)
+}
+
+function notVerified(text) {
+    interactionText.textContent = text
+    modalAlert.style.display = "block"
+}
+
 buttonOut.onclick = function() {
     modal.style.display = 'none';
-    for (const medalhinha of badges) {
-        medalhinha.setAttribute('src', './assets/img/greymedal.svg')
+    for (const badge of badges) {
+        badge.setAttribute('src', './assets/img/greymedal.svg')
     }
     points.textContent = 0
     for (let i =0; i < 6; i++) {
@@ -50,14 +66,60 @@ buttonEnd.onclick = function() {
     window.location = 'results.html'
 }
 
+buttonGiveUp.onclick = function() {
+    window.location = 'results.html'
+}
+
+buttonContinue.onclick = function() {
+    modalAlert.style.display = 'none';
+}
+
 for (const img of badges) {
     img.addEventListener('click', function setGame() {
         let spoonCondition = document.querySelectorAll('.spoon')
         if (img.src === "file:///D:/Asus/Documents/Reprograma/projeto_spoonie/assets/img/greymedal.svg") {
             spoonCondition.length > 0 ? addPointChangeBadge(img) : alertMissingCurrency()
+            console.log(selected)
         } else {
             removePointChangeBadge(img)
             addSpoon()
+            let taskValue = img.getAttribute('value')
+            if (selected.includes(taskValue)) {
+                let index = selected.indexOf(taskValue)
+                selected.splice(index, 1)
+                console.log(selected)
+            } else {
+                console.log('não tinha')
+            }
         }
     })
 }
+
+tryWin.addEventListener('click', function checkOptions() {
+    if (selected.length < 6) {
+        notVerified('Você ainda tem colheres que pode utilizar!')
+    } else {
+        switch (false) {
+            case verify("levantou"):
+                notVerified('Você não levantou da cama hoje');
+                break;
+            case verify("saiu"):
+                notVerified('Não deu pra sair de casa hoje, mas tudo bem! Você pode tentar novamente amanhã!');
+                break;
+            case verify("remédios"):
+                notVerified('Você não tomou seus remédios hoje. Além de ser perigoso, você perderá os próximos 2 dias por falta de energia');
+                break;
+            case verify("comeu"):
+                notVerified('Você esqueceu de comer e por isso passou mal. Tente se alimentar, você vai se sentir melhor!');
+                break;
+            case verify('produziu'):
+                notVerified('Não deu pra trabalhar direito e seu chefe te deu bronca. É bem ruim quando o mundo não entende sua doença crônica, não é?')
+                break;
+            case verify('exercícios'):
+                notVerified('Sem praticar exercícios seus níveis de serotonina ficaram muito baixos. Descanse por hoje e tente novamente amanhã. Talvez seja hora de procurar seu médico!');
+                break;
+            default:
+                window.location = 'results.html'
+        }
+    }
+})
